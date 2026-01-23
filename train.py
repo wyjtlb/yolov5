7d57543c6f311d6e95e6cd0a0ccfa6d25f7701dc
wuyongjie <wyj@stu.hunau.edu.cnj>
2026-01-23 18:20:25 +0800

    ci:修改amp处的代码写法

--- train.py
+++ train.py
@@ -355,7 +355,7 @@ def train(hyp, opt, device, callbacks):
     maps = np.zeros(nc)  # mAP per class
     results = (0, 0, 0, 0, 0, 0, 0)  # P, R, mAP@.5, mAP@.5-.95, val_loss(box, obj, cls)
     scheduler.last_epoch = start_epoch - 1  # do not move
-    scaler = torch.cuda.amp.GradScaler(enabled=False)
+    scaler = torch.amp.GradScaler('cuda')
     stopper, stop = EarlyStopping(patience=opt.patience), False
     compute_loss = ComputeLoss(model)  # init loss class
     callbacks.run("on_train_start")
